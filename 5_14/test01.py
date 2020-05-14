068b3cfa1e65f93380e7d8af60f705e43a6ba8ed
CindyTang12 <txdgz0624@gmail.com>
2020-05-14 16:55:36 +0800

    5_14 learning

---
+++ 5_14/test01.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://python123.io/ws/demo.html'
+
+
+def getText(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        print('Fail')
+
+
+def getChildren(soup):
+    count = 1
+    for child in soup.body.children:
+        print(count, ":", child)
+        count = count + 1
+
+
+def getParents(soup):
+    count = 1
+    for parent in soup.a.parents:
+        if parent is None:
+            print(count, ":", parent)
+        else:
+            print(count, ":", parent.name)
+        count = count + 1
+
+
+def getSibling(soup):
+    count = 1
+    for sibling in soup.a.next_siblings:
+        print(count, ":", sibling)
+        count = count + 1
+
+
+demo = getText(url)
+soup = BeautifulSoup(demo, 'html.parser')
+getChildren(soup)
+print("================================")
+getParents(soup)
+print("================================")
+getSibling(soup)
